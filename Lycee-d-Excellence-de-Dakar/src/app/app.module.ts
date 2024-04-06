1bfc4129701d0fbcecf5569343c62658d20e3135
mstfandiaye <mstfa.ndiaye23@gmail.com>
2024-04-06 16:23:59 +0000

    generate component

--- Lycee-d-Excellence-de-Dakar/src/app/app.module.ts
+++ Lycee-d-Excellence-de-Dakar/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddClassesComponent } from './add-classes/add-classes.component';
+import { AddStudentsComponent } from './add-students/add-students.component';
+import { ViewClassesComponent } from './view-classes/view-classes.component';
+import { ViewStudentsComponent } from './view-students/view-students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddClassesComponent,
+    AddStudentsComponent,
+    ViewClassesComponent,
+    ViewStudentsComponent
   ],
   imports: [
     BrowserModule,
---
+++ Lycee-d-Excellence-de-Dakar/src/app/view-classes/view-classes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-classes',
+  templateUrl: './view-classes.component.html',
+  styleUrl: './view-classes.component.css'
+})
+export class ViewClassesComponent {
+
+}
