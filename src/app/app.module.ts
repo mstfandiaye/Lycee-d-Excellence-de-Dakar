2afca6e3349f332533a8b287ee4c1413997226eb
mstfandiaye <mstfa.ndiaye23@gmail.com>
2024-05-01 16:13:18 +0000

    component an service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassesComponent } from './classes/classes.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassesComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
