b95808fe8cdf95cd1760a735684d4c27b2de029e
mstfandiaye <mstfa.ndiaye23@gmail.com>
2024-05-05 15:10:56 +0000

    barside

--- src/app/side-bare/side-bare.component.ts
+++ src/app/side-bare/side-bare.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-side-bare',
   templateUrl: './side-bare.component.html',
   styleUrl: './side-bare.component.css'
 })
-export class SideBareComponent {
+export class SideBareComponent  {
   
 
-}
+  
+  }
+
--- src/app/side-bare/sidebare.service.ts
+++ src/app/side-bare/sidebare.service.ts
@@ -5,16 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class SidebareService {
 
-  constructor() { }
-
-  toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar') as HTMLElement;
-
-    if (sidebar.style.left === '-250px') {
-      sidebar.style.left = '0';
-    } else {
-      sidebar.style.left = '-250px';
-    }
-  }
+  
 
 }
